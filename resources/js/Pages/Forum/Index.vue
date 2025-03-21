8efe38f357ad7f485bd32e8139163bad28abe63d
Peter Pike <pikepeter@gmail.com>
2025-03-21 23:07:37 +1200

    forum layout created

--- resources/js/Pages/Forum/Index.vue
+++ resources/js/Pages/Forum/Index.vue
@@ -1,24 +1,26 @@
 <script setup>
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import ForumLayout from '@/Layouts/ForumLayout.vue';
 import { Head } from '@inertiajs/vue3';
 </script>
 
 <template>
     <Head title="Forum" />
 
-    <AuthenticatedLayout>
-
-
-        <div class="py-12">
-            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+    <ForumLayout>  
                 <div
-                    class="overflow-hidden bg-white shadow-sm sm:rounded-lg"
-                >
+                    class="overflow-hidden bg-white shadow-sm sm:rounded-lg" >
                     <div class="p-6 text-gray-900">
-                        
                     </div>
                 </div>
-            </div>
-        </div>
-    </AuthenticatedLayout>
+        
+        
+        <template #side>
+            <div
+                    class="overflow-hidden bg-white shadow-sm sm:rounded-lg" >
+                    <div class="p-6 text-gray-900">
+                        Side
+                    </div>
+                </div>
+        </template>
+    </ForumLayout>
 </template>
