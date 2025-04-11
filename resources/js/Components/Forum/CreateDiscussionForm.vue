6761aa9e438d897cef83b7e7dd9b0e7044a824a7
Peter Pike <pikepeter@gmail.com>
2025-04-12 07:06:46 +1000

    using components before the text area, input and select

--- resources/js/Components/Forum/CreateDiscussionForm.vue
+++ resources/js/Components/Forum/CreateDiscussionForm.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 import FixedFormWrapper from './FixedFormWrapper.vue';
-
+import TextInput from '../TextInput.vue';
+import InputLabel from '../InputLabel.vue';
+import InputError from '../InputError.vue';
+import SelectTopic from '../SelectTopic.vue';
 </script>
 
 <template>
@@ -12,20 +15,33 @@ import FixedFormWrapper from './FixedFormWrapper.vue';
             <template v-slot:main>
                 <div class="flex items-start space-x-3">
                     <div class="flex-grow">
-                        <input type="text" name="" class="w-full">
+                        <div>
+                            <InputLabel for="title" value="Tilte" class="sr-only"/>
+
+                            <TextInput id="title" type="text" class="w-full" />
+
+                            <!-- <InputError class="mt-2"  /> -->
+                        </div>
+
                     </div>
                     <div>
-                        <select >
-                            <option value="">Laravel</option>
-                        </select>
+                        <SelectTopic id="topic">
+                            <option value="">Choose a Topic</option>
+                            <option 
+                                :value="topic.slug" 
+                                v-for="topic in $page.props.topics" 
+                                :key="topic.id">
+                                {{ topic.title }}
+                            </option>
+                        </SelectTopic>
                     </div>
-                </div> 
-                <div>
+                </div>
+                <div class="mt-2">
                     <textarea name="" rows="4" id="" class="w-full" />
                 </div>
             </template>
             <template v-slot:button>
-                <button type="submit">Create </button> 
+                <button type="submit">Create </button>
             </template>
 
 
