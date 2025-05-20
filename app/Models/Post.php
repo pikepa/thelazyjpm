e819ff29f4c3c482b50219c06bf4ef716772847a
Peter Pike <pikepeter@gmail.com>
2025-05-20 12:06:26 +0400

    updating git hp, to transfer to mac to try and identify why mentions user_id is not being populated

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,12 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Post extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['body'];
+    protected $fillable = ["body"];
 
     public function discussion(): BelongsTo
     {
@@ -24,6 +25,15 @@ class Post extends Model
 
     public function parent(): BelongsTo
     {
-        return $this->belongsTo(Post::class, 'parent_id');
+        return $this->belongsTo(Post::class, "parent_id");
+    }
+
+    public function mentions(): BelongsToMany
+    {
+        return $this->belongsToMany(
+            Post::class,
+            "mentions",
+            "post_id"
+        )->withTimestamps();
     }
 }
--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,9 +2,9 @@
 
 namespace App\Providers;
 
-// use App\Models\Post;
+use App\Models\Post;
 use App\Models\User;
-// use App\Observers\PostObserver;
+use App\Observers\PostObserver;
 use App\Observers\UserObserver;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
@@ -15,7 +15,7 @@ class EventServiceProvider extends ServiceProvider
 {
     protected $observers = [
         User::class => [UserObserver::class],
-        // Post::class => [PostObserver::class],
+        Post::class => [PostObserver::class],
     ];
 
     /**
