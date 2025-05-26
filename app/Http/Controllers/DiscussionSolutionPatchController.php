f17dfd724f7f0b8752b873880cf8e15b6452d0a8
Peter Pike <pikepeter@gmail.com>
2025-05-26 20:10:08 +0400

    check that the deleted posts exists within a discussion - ENd of course

--- app/Http/Controllers/DiscussionSolutionPatchController.php
+++ app/Http/Controllers/DiscussionSolutionPatchController.php
@@ -8,10 +8,14 @@ use App\Models\Post;
 
 class DiscussionSolutionPatchController extends Controller
 {
-    public function __invoke(DiscussionSolutionPatchRequest $request, Discussion $discussion)
-    {
+    public function __invoke(
+        DiscussionSolutionPatchRequest $request,
+        Discussion $discussion
+    ) {
         //Make sure post_id is within this topic
-        $discussion->solution()->associate(Post::find($request->post_id));
+        $discussion
+            ->solution()
+            ->associate($discussion->posts()->find($request->post_id));
         $discussion->save();
     }
 }
