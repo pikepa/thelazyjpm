06c3f694c43bd20353dbd510a663909b977ffe13
Peter Pike <pikepeter@gmail.com>
2025-05-26 06:43:29 +0400

    added mentions to the create post form

--- resources/js/Components/Forum/CreatePostForm.vue
+++ resources/js/Components/Forum/CreatePostForm.vue
@@ -1,55 +1,71 @@
-
-
 <template>
     <div>
-        <FixedFormWrapper v-if="visible" v-on:submit.prevent="createPost" :form="form">
+        <FixedFormWrapper
+            v-if="visible"
+            v-on:submit.prevent="createPost"
+            :form="form"
+        >
             <template v-slot:header>
                 <div class="flex items-center justify-between">
-                    <h1 class="text-lg font-medium">Replying to {{ discussion.title }}</h1>
+                    <h1 class="text-lg font-medium">
+                        Replying to {{ discussion.title }}
+                    </h1>
                     <button v-on:click="hideCreatePostForm">
-                        <Svg class="h-5 w-5" name='icon-close' ></Svg>
+                        <Svg class="h-5 w-5" name="icon-close"></Svg>
                     </button>
                 </div>
             </template>
 
             <template v-slot:main="{ markdownPreviewEnabled }">
-                <div >
+                <div>
                     <InputLabel for="body" value="Body" class="sr-only" />
-                    <TextArea id='body' v-if="!markdownPreviewEnabled" class="w-full h-48 align-top" v-model="form.body"/>
-                    <InputError class="mt-2" :message="form.errors.body" />
+                    <Mentionable
+                        :keys="['@']"
+                        offset="6"
+                        v-on:search="mentionSearch"
+                        :items="mentionSearchResults"
+                    >
+                        <TextArea
+                            id="body"
+                            class="w-full h-48 align-top"
+                            v-model="form.body"
+                        />
 
+                        <template #no-result>
+                            <div class="mention-item">No Username found</div>
+                        </template>
+                    </Mentionable>
+                    <InputError class="mt-2" :message="form.errors.body" />
                 </div>
             </template>
             <template v-slot:button>
-                <PrimaryButton >
-                    Reply
-                </PrimaryButton>
+                <PrimaryButton> Reply </PrimaryButton>
             </template>
-
-
         </FixedFormWrapper>
     </div>
 </template>
 
 <script setup lang="ts">
-import FixedFormWrapper from './FixedFormWrapper.vue';
-import InputLabel from '../InputLabel.vue';
-import InputError from '../InputError.vue';
-import PrimaryButton from '@/Components/PrimaryButton.vue';
-import TextArea from '../TextArea.vue';
-import useCreatePost from '@/Composables/useCreatePost.js';
-import { useForm } from '@inertiajs/vue3';
-import Svg from '../Svg.vue';
+import FixedFormWrapper from "./FixedFormWrapper.vue";
+import InputLabel from "../InputLabel.vue";
+import InputError from "../InputError.vue";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
+import TextArea from "../TextArea.vue";
+import useCreatePost from "@/Composables/useCreatePost.js";
+// import { useForm } from "@inertiajs/vue3";
+import Svg from "../Svg.vue";
+import { Mentionable } from "vue-mention";
+import useMentionSearch from "@/Composables/useMentionSearch";
 
-const { visible , hideCreatePostForm, form, discussion} = useCreatePost()
+const { visible, hideCreatePostForm, form, discussion } = useCreatePost();
+const { mentionSearch, mentionSearchResults } = useMentionSearch();
 
 const createPost = () => {
- 
-  form.post(route('posts.store', discussion.value),{
-    onSuccess: () => {
-        form.reset()
-        hideCreatePostForm()
-    }
-  })
-}
+    form.post(route("posts.store", discussion.value), {
+        onSuccess: () => {
+            form.reset();
+            hideCreatePostForm();
+        },
+    });
+};
 </script>
