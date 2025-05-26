52c42c8662087a3d78708eaf66876bfab0190715
Peter Pike <pikepeter@gmail.com>
2025-05-26 19:33:19 +0400

    ammended vite for server rendering

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,21 @@
-import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
-import svgLoader from 'vite-svg-loader'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
+import svgLoader from "vite-svg-loader";
 
 export default defineConfig({
+    ssr: {
+        noExternal: ["vue-mention", "floating-vue"],
+    },
     plugins: [
         tailwindcss(),
         svgLoader({
-            svgo: false
+            svgo: false,
         }),
         laravel({
-            input: 'resources/js/app.js',
-            ssr: 'resources/js/ssr.js',
+            input: "resources/js/app.js",
+            ssr: "resources/js/ssr.js",
             refresh: true,
         }),
         vue({
