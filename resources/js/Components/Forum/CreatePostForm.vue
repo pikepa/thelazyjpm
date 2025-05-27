d72e190b8cdfd9efd38fa376065ed35a60598956
Peter Pike <pikepeter@gmail.com>
2025-05-27 18:58:02 +0400

    replaced CreatePost form

--- resources/js/Components/Forum/CreatePostForm.vue
+++ resources/js/Components/Forum/CreatePostForm.vue
@@ -1,13 +1,59 @@
-<script setup lang="ts">
+<template>
+    <FixedFormWrapper
+        v-if="visible"
+        v-on:submit.prevent="createPost"
+        :form="form"
+    >
+        <template v-slot:header>
+            <div class="flex items-center justify-between">
+                <h1 class="text-lg font-medium">
+                    Replying to {{ discussion.title }}
+                </h1>
+                <button v-on:click="hideCreatePostForm">
+                    <Svg name="icon-close" class="h-5 w-5"></Svg>
+                </button>
+            </div>
+        </template>
+
+        <template v-slot:main="{ markdownPreviewEnabled }">
+            <div>
+                <InputLabel for="body" value="Body" class="sr-only" />
+                <Mentionable
+                    :keys="['@']"
+                    offset="6"
+                    v-on:search="mentionSearch"
+                    :items="mentionSearchResults"
+                    v-if="!markdownPreviewEnabled"
+                >
+                    <Textarea
+                        id="body"
+                        class="w-full h-48 align-top"
+                        v-model="form.body"
+                    />
+
+                    <template #no-result>
+                        <div class="mention-item">No username found</div>
+                    </template>
+                </Mentionable>
+                <InputError class="mt-2" :message="form.errors.body" />
+            </div>
+        </template>
+
+        <template v-slot:button>
+            <PrimaryButton> Reply </PrimaryButton>
+        </template>
+    </FixedFormWrapper>
+</template>
+
+<script setup>
 import FixedFormWrapper from "./FixedFormWrapper.vue";
 import InputLabel from "../InputLabel.vue";
 import InputError from "../InputError.vue";
-import PrimaryButton from "@/Components/PrimaryButton.vue";
-import TextArea from "../TextArea.vue";
-import useCreatePost from "@/Composables/useCreatePost.js";
-// import { useForm } from "@inertiajs/vue3";
+import PrimaryButton from "../PrimaryButton.vue";
+import Textarea from "../Textarea.vue";
+import useCreatePost from "@/Composables/useCreatePost";
 import Svg from "../Svg.vue";
-import Mentionable from "vue-mention";
+import { Mentionable } from "vue-mention";
 import useMentionSearch from "@/Composables/useMentionSearch";
 import { watch } from "vue";
 
@@ -18,7 +64,8 @@ watch(user, (user) => {
     if (!user) {
         return;
     }
-    form.body = `@${user.username} ` + form.body;
+
+    form.body = `@${user.username}` + form.body;
 });
 
 const createPost = () => {
@@ -30,50 +77,3 @@ const createPost = () => {
     });
 };
 </script>
-
-<template>
-    <div>
-        <FixedFormWrapper
-            v-if="visible"
-            v-on:submit.prevent="createPost"
-            :form="form"
-        >
-            <template v-slot:header>
-                <div class="flex items-center justify-between">
-                    <h1 class="text-lg font-medium">
-                        Replying to {{ discussion.title }}
-                    </h1>
-                    <button v-on:click="hideCreatePostForm">
-                        <Svg class="h-5 w-5" name="icon-close"></Svg>
-                    </button>
-                </div>
-            </template>
-
-            <template v-slot:main="{ markdownPreviewEnabled }">
-                <div>
-                    <InputLabel for="body" value="Body" class="sr-only" />
-                    <Mentionable
-                        :keys="['@']"
-                        offset="6"
-                        v-on:search="mentionSearch"
-                        :items="mentionSearchResults"
-                    >
-                        <TextArea
-                            id="body"
-                            class="w-full h-48 align-top"
-                            v-model="form.body"
-                        />
-
-                        <template #no-result>
-                            <div class="mention-item">No Username found</div>
-                        </template>
-                    </Mentionable>
-                    <InputError class="mt-2" :message="form.errors.body" />
-                </div>
-            </template>
-            <template v-slot:button>
-                <PrimaryButton> Reply </PrimaryButton>
-            </template>
-        </FixedFormWrapper>
-    </div>
-</template>
